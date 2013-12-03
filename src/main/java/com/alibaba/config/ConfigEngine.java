f6d30f1a9140590c939df81f014f8878a7e4d99c
ixrwang <mail@ixr.name>
2013-12-03 17:54:27 +0800

    ...

---
+++ src/main/java/com/alibaba/config/ConfigEngine.java
@@ -0,0 +1,15 @@
+package com.alibaba.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ConfigEngine : TODO: yuuji
+ * yuuji 5:42 PM 11/29/13
+ */
+public class ConfigEngine {
+    private static Map<String,PageConfig> pageConfigMap = new HashMap<String, PageConfig>();
+
+
+
+}
