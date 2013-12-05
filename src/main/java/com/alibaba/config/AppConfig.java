7dd94f37be0202b0d9f19fa2c083675e38c65edf
ixrwang <mail@ixr.name>
2013-12-05 16:03:59 +0800

    ...

---
+++ src/main/java/com/alibaba/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.alibaba.config;
+
+import java.io.File;
+
+/**
+ * AppConfig : TODO: yuuji
+ * yuuji 11:27 AM 12/5/13
+ */
+public class AppConfig extends BaseConfig{
+
+}
---
+++ src/main/java/com/alibaba/config/LayoutConfig.java
@@ -0,0 +1,8 @@
+package com.alibaba.config;
+
+/**
+ * LayoutConfig : TODO: yuuji
+ * yuuji 2:04 PM 12/5/13
+ */
+public class LayoutConfig extends BaseConfig{
+}
