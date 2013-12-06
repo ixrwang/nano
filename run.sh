2386cf8f55c63bf242e64db3d179577e8746b6b4
ixrwang <mail@ixr.name>
2013-12-06 09:36:58 +0800

    ...

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 main=com.alibaba.HttpServer
-port=8080
+port=80
+log=target/logs.txt
 pids=`ps gx | grep $main | grep $port | awk '{print $1}'`
 for pid in $pids
 do
@@ -7,5 +8,5 @@ kill -9 $pid
 echo kill $pid
 done
 mvn clean compile
-rm -rf logs.txt
-nohup mvn exec:java -Dexec.mainClass="$main" -Dexec.args="$port" > logs.txt &
+rm -rf $log
+nohup mvn exec:java -Dexec.mainClass="$main" -Dexec.args="$port" > $log &
