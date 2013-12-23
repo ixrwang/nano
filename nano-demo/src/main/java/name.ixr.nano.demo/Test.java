f8bd5976f6df3acb6f287f7b080865e28058911c
ixr_wang <mail@ixr.name>
2013-12-23 20:36:44 +0800

    ...

--- nano-demo/src/main/java/name.ixr.nano.demo/Test.java
+++ nano-demo/src/main/java/name.ixr.nano.demo/Test.java
@@ -1,12 +1,13 @@
 package name.ixr.nano.demo;
 
 import name.ixr.nano.common.utils.ResourceUtils;
+import name.ixr.nano.engine.DefaultEngine;
 
 /**
  * Created by yuuji on 12/16/13.
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        ResourceUtils.getURL("app/demo.vm");
+        new DefaultEngine(80).run();
     }
 }
